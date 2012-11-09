d9738e3a0e137300ebe5b90a7b0ea24b797546fa
Kiwamu Okabe <kiwamu@debian.or.jp>
2012-11-09 21:57:09 +0900

    split normal function and memo function

--- RpnCalcState/RpnCalcState.hs
+++ RpnCalcState/RpnCalcState.hs
@@ -1,14 +1,19 @@
+foldingFunction :: [Float] -> String -> [Float]
+foldingFunction (x:y:ys) "*" = (x * y):ys
+foldingFunction (x:y:ys) "+" = (x + y):ys
+foldingFunction (x:y:ys) "-" = (y - x):ys
+foldingFunction (x:y:ys) "/" = (y / x):ys
+foldingFunction (x:y:ys) "^" = (y ** x):ys
+foldingFunction (x:xs) "ln" = log x:xs
+foldingFunction (xs) "sum" = [sum xs]
+foldingFunction (xs) numberString = read numberString:xs
+
+foldingFunctionM :: ([Float], Float) -> String -> ([Float], Float)
+foldingFunctionM (x:xs, m) "M+" = (x:xs, m + x)
+foldingFunctionM (x:xs, m) "M-" = (x:xs, m - x)
+foldingFunctionM (xs, _) "MC" = (xs, 0)
+foldingFunctionM (xs, m) "MR" = (m:xs, m)
+foldingFunctionM (x, m) s = (foldingFunction x s, m)
+
 solveRPN :: String -> Float
-solveRPN = head . fst . foldl foldingFunction ([], 0.0) . words
-  where foldingFunction (x:y:ys, m) "*" = ((x * y):ys, m)
-        foldingFunction (x:y:ys, m) "+" = ((x + y):ys, m)
-        foldingFunction (x:y:ys, m) "-" = ((y - x):ys, m)
-        foldingFunction (x:y:ys, m) "/" = ((y / x):ys, m)
-        foldingFunction (x:y:ys, m) "^" = ((y ** x):ys, m)
-        foldingFunction (x:xs, m) "ln" = (log x:xs, m)
-        foldingFunction (xs, m) "sum" = ([sum xs], m)
-        foldingFunction (x:xs, m) "M+" = (x:xs, m + x)
-        foldingFunction (x:xs, m) "M-" = (x:xs, m - x)
-        foldingFunction (xs, _) "MC" = (xs, 0)
-        foldingFunction (xs, m) "MR" = (m:xs, m)
-        foldingFunction (xs, m) numberString = (read numberString:xs, m)
+solveRPN = head . fst . foldl foldingFunctionM ([], 0.0) . words
